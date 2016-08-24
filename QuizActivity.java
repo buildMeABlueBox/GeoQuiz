bd2572f0d9598572e9fb004040484bfd20f68fb9
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2016-08-24 19:34:00 -0400

    made a previous button to go back to previous questions.

--- QuizActivity.java
+++ QuizActivity.java
@@ -14,6 +14,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_africa, false),
@@ -56,6 +57,15 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = mCurrentIndex >0 ? (mCurrentIndex-1) % mQuestionBank.length : mCurrentIndex;
                 updateQuestion();
             }
         });
